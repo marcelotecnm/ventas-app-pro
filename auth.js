5ceae87f7b97a249dcca5c891931339735ba3656
marcelotecnm <marmaiden1988@gmail.com>
2026-03-28 12:56:48 -0600

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,48 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCVRXFJZSeC-wzIwqqN8xPc828i9LavaMo",
+    authDomain: "ventas-app-pro-c5bb0.firebaseapp.com",
+    projectId: "ventas-app-pro-c5bb0",
+    storageBucket: "ventas-app-pro-c5bb0.firebasestorage.app",
+    messagingSenderId: "446489860713",
+    appId: "1:446489860713:web:5166ef041fec81481420f8"
+  };
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// REGISTRO
+window.registrar = async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    alert("Usuario registrado correctamente");
+    window.location.href = "login.html";
+  } catch (error) {
+    alert("Error: " + error.message);
+  }
+};
+
+// LOGIN
+window.login = async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    alert("Login exitoso");
+    window.location.href = "index.html"; // redirige al CRUD
+  } catch (error) {
+    alert("Error: " + error.message);
+  }
+};
